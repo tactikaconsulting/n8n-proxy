13deb2fed51720b16924b0a6a5157841d49f9f6a
tactikaconsulting <tactikaconsulting1@gmail.com>
2025-08-09 11:36:40 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.tactikaconsulting.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
